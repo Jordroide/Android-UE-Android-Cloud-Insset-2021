4dba4824eff82e2eb8d1beb4c78bcd679fd950d7
Jordroid <jordaneserreau@gmail.com>
2021-09-13 10:51:43 +0200

    Implement activity with intent
    Add view binding support
    Implement 2 new acitivity with layout

--- app/src/main/java/com/jordroid/android/cloud2021/MainActivity.kt
+++ app/src/main/java/com/jordroid/android/cloud2021/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.jordroid.android.cloud2021
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jordroid.android.cloud2021.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.mainButtonActivityIntent.setOnClickListener {
+            goToActivityIntent()
+        }
+    }
+
+    /**
+     * Redirect to the new activity
+     */
+    private fun goToActivityIntent() {
+        startActivity(Intent(this, FirstActivity::class.java))
     }
 }
---
+++ app/src/main/java/com/jordroid/android/cloud2021/SecondActivity.kt
@@ -0,0 +1,20 @@
+package com.jordroid.android.cloud2021
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.jordroid.android.cloud2021.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Get the xml layout to attach to this activity
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val valueFromIntent = intent?.extras?.getString("MY_KEY")
+        binding.secondActivityText.text = valueFromIntent
+    }
+}
